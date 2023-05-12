5343163fac9568dd207037f39578e580d589b700
fbrand-new <francesco.brand@iit.it>
2023-05-12 17:10:36 +0200

    Base unit test working

--- yarpPose/yarpMMPose.py
+++ yarpPose/yarpMMPose.py
@@ -1,16 +1,52 @@
 from yarpPose import PoseEstimation
 from mmpose.apis import MMPoseInferencer
+import importlib.util
+import sys
 
 class yarpMMPose(PoseEstimation):
 
-    def __init__(self,alias):
-        self.inferencer = MMPoseInferencer(alias)
+    def __init__(self,poseInferencer,dataset='COCO'):
+        self.inferencer = poseInferencer
+        self.dataset = dataset
+   
+    @classmethod
+    def fromalias(cls,alias='human',dataset='COCO'):
+        return cls(MMPoseInferencer(alias),dataset)
+    
+    @classmethod
+    def fromconfig(cls,config,model,det_model=None,det_weights=None,dataset='COCO'):
+        if det_model and det_weights:
+            print(det_weights)
+            inferencer = cls(MMPoseInferencer(config,model,det_model=det_model,det_weights=det_weights),dataset)
+        else:
+            inferencer = cls(MMPoseInferencer(config,model),dataset)
 
-    def __init__(self,config,model):
-        self.inferencer = MMPoseInferencer(config,model)
+        return inferencer
 
     def inference(self,img):
-        return self.inferencer(img)
+        generator = self.inferencer(img)
+        result = next(generator)
+
+        if self.dataset == 'COCO':
+            spec = importlib.util.spec_from_file_location("coco",'/mmpose/configs/_base_/datasets/coco.py')
+            dataset_module = importlib.util.module_from_spec(spec)
+            sys.modules["coco"] = dataset_module
+            spec.loader.exec_module(dataset_module)
+
+        keypoint_info = dataset_module.dataset_info["keypoint_info"]
+
+        keypoints = [] 
+        predictions = result["predictions"]
+
+        for person in predictions[0]: 
+            person_keypoints = {} 
+            for i, keypoint in enumerate(person["keypoints"]):
+                person_keypoints[keypoint_info[i]["name"]] = keypoint 
+                person_keypoints[keypoint_info[i]["name"]].append(person["keypoint_scores"][i])
+
+            keypoints.append(person_keypoints)
+        
+        return keypoints
 
     # def configure():
     #     - configure the network
--- yarpPose/yarpPose.py
+++ yarpPose/yarpPose.py
@@ -1,3 +1,6 @@
+import numpy as np
+import yarp
+
 class PoseEstimation():
 
     def __init__():
@@ -6,3 +9,38 @@ class PoseEstimation():
     def inference():
         pass
 
+    def configure():
+        pass
+
+        image_h = READ
+        image_w = READ
+
+        yarp.Network.init()
+
+        in_port = yarp.BufferedPortImageRgb()
+        in_port_name = %%MODULENAME%%+"/image:i"
+        in_port.open(in_port_name)
+
+        out_port = yarp.BufferedPortImageRgb()
+        out_port_name = %%MODULENAME%%+"/image:o"
+        out_port.open(in_port_name)
+
+
+        in_buf_array = np.ones((image_h,image_w,3), dtype = np.uint8)
+        in_buf_image = yarp.ImageRgb()
+        in_buf_image.resize(image_w,image_h)
+        in_buf_image.setExternal(in_buf_array,in_buf_array.shape[1],in_buf_array.shape[1])
+
+        out_buf_array = np.ones((image_h,image_w,3), dtype = np.int8)
+        out_buf_image = yarp.ImageRgb()
+        out_buf_image.resize(image_w,image_h)
+        out_buf_image.setExternal(out_buf_array,out_buf_array.shape[1],out_buf_array.shape[1])
+
+        while True:
+
+            received_image = in_port.read()
+            in_buf_image.copy(received_image)
+
+            assert in_buf_array.__array_interface__['data'][0] == in_buf_image.getRawImage().__int__()
+
+            
